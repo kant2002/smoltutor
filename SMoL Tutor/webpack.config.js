3a9d82ee1f618398572b5c0718f82c99bbb33525
Andrii Kurdiumov <kant2002@gmail.com>
2024-06-07 18:55:37 +0500

    Більше реверсінгу

--- SMoL Tutor/webpack.config.js
+++ SMoL Tutor/webpack.config.js
@@ -1,13 +1,23 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.jsx',
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: 'webpack.bundle.js',
     },
     module: {
       rules: [
+        {
+          test: /.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options:{
+             presets: ["@babel/preset-env", "@babel/preset-react"],
+            }
+          }
+        },
         {
           test: /\.css$/i,
           use: ["style-loader", "css-loader"],
